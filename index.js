a7d53c82a5152369414f6d0bba8450366c8e3d0a
Faith Makinde <faithmakinde0@gmail.com>
2022-10-04 23:51:13 +0100

    rendering elements

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 import { tweetsData } from "/data.js";
-console.log(tweetsData);
+
+const btn =  document.getElementById("tweet-btn");
+const txtArea = document.getElementById("txt")
+
+
+
+function getFeedHtml(tweets){
+    let feedHtml = "";
+       tweets.forEach(tweet => {
+        feedHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+        </div>
+        `
+        
+           })
+          return feedHtml;
+        }
+     
+
+        
+
